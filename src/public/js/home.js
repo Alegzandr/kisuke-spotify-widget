6312e9784e7c4ca48e912a6bda3f70844e6f20df
Alexandre Farrenq <alexandre1.farrenq@epitech.eu>
2023-12-09 20:52:13 +0100

    Add clear button functionality to remove recent overlays

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -6,10 +6,22 @@ window.addEventListener('load', () => {
     const recentOverlaysElement = document.querySelector('#overlays');
     const recentOverlaysListElement = document.querySelector('#overlays-list');
     const loginButtonElement = document.querySelector('#login');
+    const clearButtonElement = document.querySelector('#clear');
+
+    const clearList = (event) => {
+        event.preventDefault();
+
+        localStorage.removeItem('recentOverlays');
+        location.reload();
+    };
+
+    clearButtonElement.addEventListener('click', clearList);
 
     if (recentOverlays.length > 0) {
         loginButtonElement.innerText = 'Se reconnecter avec Spotify';
         recentOverlaysElement.style.display = 'block';
+
+        recentOverlays.reverse();
 
         recentOverlays.forEach((overlayId) => {
             const li = document.createElement('li');
